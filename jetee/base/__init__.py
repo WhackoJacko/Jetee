6de1a7c5c75b136e06201561e391f0c08e459ed6
Сергей Дубинин <whackojacko@Mac-mini-Sergej.local>
2014-08-15 19:04:50 +0600

    setup.py fixes

--- jetee/base/__init__.py
+++ jetee/base/__init__.py
@@ -1,1 +1,4 @@
-__author__ = 'whackojacko'
+REQUIRED_ANSIBLE_ROLES = (
+    u'bennojoy.nginx',
+    u'EDITD.supervisor_task',
+)
--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-from os.path import join, dirname
+import subprocess
 
+from setuptools.command.install import install
+from setuptools.command.develop import develop
 from setuptools import setup
+
+
+def galaxy_packages_wrapper(cls):
+    def run(self):
+        original_run(self)
+        from jetee import base
+
+        subprocess.call([u'ansible-galaxy', 'install'] + list(base.REQUIRED_ANSIBLE_ROLES))
+
+    original_run = cls.run
+    cls.run = run
+    return cls
 
 
 setup(
@@ -8,6 +22,11 @@ setup(
     version=0.3,
     # long_description=open(join(dirname(__file__), 'README.md')).read(),
     author='WhackoJacko',
+    cmdclass={
+        'install': galaxy_packages_wrapper(install),
+        u'develop': galaxy_packages_wrapper(develop)
+    },
+
     entry_points={
         'console_scripts': [
             'jetee = jetee.runtime.app:dispatcher.run',
