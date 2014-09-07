fa24996e1140db2d25ce08a63cbfa7787456d8a3
WhackoJacko <whackojacko@MacBook-Pro-Sergej.local>
2014-09-07 21:11:01 +0600

    fix processes

--- jetee/project/processes.py
+++ jetee/project/processes.py
@@ -8,17 +8,37 @@ class CustomProcess(ProcessAbstract):
         return self.command
 
 
+class PythonProcess(ProcessAbstract):
+    def __init__(self, executable):
+        self.executable = executable
+
+    def get_command(self):
+        return u'python %s' % self.executable
+
+
 class UWSGIProcess(ProcessAbstract):
     port = 9000
 
-    def __init__(self, wsgi_file):
+    def __init__(self, wsgi_file=None, wsgi_module=None, processes=None, threads=None):
         self.wsgi_file = wsgi_file
+        self.processes = processes
+        self.threads = threads
+        self.wsgi_module = wsgi_module
 
     def get_name(self):
         return u'web_server'
 
     def get_command(self):
-        return u'uwsgi --http :%i --wsgi-file %s' % (self.port, self.wsgi_file)
+        command = u'uwsgi --http :%i' % self.port
+        if self.wsgi_module:
+            command += u' --wsgi %s' % self.wsgi_module
+        if self.wsgi_file:
+            command += u' --wsgi-file %s' % self.wsgi_file
+        if self.processes:
+            command += u' --processes %i' % self.processes
+        if self.threads:
+            command += u' --threads %i' % self.threads
+        return command
 
 
 class DjangoGunicornProcess(ProcessAbstract):
@@ -31,7 +51,7 @@ class DjangoGunicornProcess(ProcessAbstract):
 class CeleryWorkerProcess(ProcessAbstract):
     initial_command = u'celery worker'
 
-    def __init__(self, app, queues=None, broker=None, concurrency=4, beat=False):
+    def __init__(self, app=None, queues=None, broker=None, concurrency=4, beat=False):
         self.app = app
         self.queues = queues
         self.broker = broker
