d6d93633bfc3b0023b2e18857a8529fafa661882
WhackoJacko <whackojacko.ru@gmail.com>
2014-11-09 17:46:41 +0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ else:
 
 setup(
     name='jetee',
-    version=u'0.6.3',
+    version=u'0.6.5',
     description='Lightweight deployment tool',
     author='Sergey Dubinin',
     author_email='whackojacko.ru@gmail.com',
