f441b3b27c5b1dc9b5a253dca4ed63dddb5f72a3
WhackoJacko <whackojacko.ru@gmail.com>
2015-01-16 15:38:29 +0500

    version

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ else:
 
 setup(
     name='jetee',
-    version=u'0.7',
+    version=u'0.7.1',
     description='Lightweight deployment tool',
     author='Sergey Dubinin',
     author_email='whackojacko.ru@gmail.com',
     url='https://github.com/WhackoJacko/Jetee.git',
-    download_url='https://github.com/whackojacko/jetee/tarball/v0.7/',
+    download_url='https://github.com/whackojacko/jetee/tarball/v0.7.1/',
     entry_points={
         'console_scripts': [
             'jetee = jetee.runtime.app:dispatcher.run',
